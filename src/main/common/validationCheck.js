6512be77016c51f12c73916e1a7d4887ddf157ce
R3pic <101osc@naver.com>
2024-06-25 13:18:44 +0900

    jsdoc added

--- src/main/common/validationCheck.js
+++ src/main/common/validationCheck.js
@@ -1,5 +1,11 @@
 import createHttpError from "http-errors";
+import { Result, AlternativeValidationError, GroupedAlternativeValidationError, UnknownFieldsError, FieldValidationError } from 'express-validator';
 
+/**
+ *
+ * @param {Result<AlternativeValidationError | GroupedAlternativeValidationError | UnknownFieldsError | FieldValidationError>} errors
+ * @param {NextFunction} next
+ */
 export const errorCheck = (errors, next) => {
     if (!errors.isEmpty()){
         const httpErr = createHttpError(400, "Validation Error");
