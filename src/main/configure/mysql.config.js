6ba0814207f65a4a992fa8f1bfa09d00354c3669
R3pic <101osc@naver.com>
2024-06-25 14:45:36 +0900

    mysql.config 변경

--- src/main/configure/mysql.config.js
+++ src/main/configure/mysql.config.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise.js';
 
 const conn = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1232',
-    database: 'todo',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 const open = () => {
